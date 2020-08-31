72dda7bd873725e6207967fe2b78c59e94ec9664
萌大统领 <tom_kang@qq.com>
2020-08-31 19:46:03 +0800

    Create BaseTest.java

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+@Rollback(false)
+@SpringBootTest
+public class BaseTest {
+
+
+
+}
