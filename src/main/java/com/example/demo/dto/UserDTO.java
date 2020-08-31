f23b590c5a5e02ee3bdedd203f226fcc87c7c897
萌大统领 <tom_kang@qq.com>
2020-08-31 19:54:06 +0800

    Create UserDTO.java

---
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package com.example.demo.dto;
+
+public class UserDTO {
+
+    private String name ;
+    private String address ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
