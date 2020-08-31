e02699ed2ab564cc455ae8efbe0df3b0a3e0eda6
萌大统领 <tom_kang@qq.com>
2020-08-31 19:53:42 +0800

    Create User.java

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,69 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class User implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+    @Column(name = "name")
+    String name;
+    @Column(name = "address")
+    String address;
+    @Column(name = "age")
+    Integer age;
+    @Column(name = "create_time")
+    Date createTime;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
